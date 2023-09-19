2512554f949e3c0be15617efcff25e32f4ea5816
Samir Khairati <34453746+Samirkhairati@users.noreply.github.com>
2023-09-19 19:31:21 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function submit() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username != "" && username != null) {
+        setCookie("username", username, 365);
+        if (password != "" && password != null) {
+            setCookie("password", password, 365);
+            window.location.href = "./home.html"
+        } 
+        else {
+            alert('Enter a password')
+        }
+    } 
+    else {
+        alert('Enter a username')
+    }
+    
+}
